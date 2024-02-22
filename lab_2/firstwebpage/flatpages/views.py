a5aec7ede8d83fabd8cd88570fbb6f964c220a1d
Stealr <81468563+Stealr@users.noreply.github.com>
2024-02-22 18:27:09 +0300

    Delete lab_2 directory

--- lab_2/firstwebpage/flatpages/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'static_handler.html')
